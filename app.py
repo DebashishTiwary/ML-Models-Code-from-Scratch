abcbbd680212bfeabebcab4e50939ad57bcc0517
Debashish <debasishtewary5@gmail.com>
2026-09-20 03:33:43 +0530

    minor changes

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import streamlit as st
 
 st.set_page_config(page_title="Home Page", layout="wide")
 
-st.title("Machine Learning Models Dashboard")
+st.title("Machine Learning Model Codes from Scratch")
 st.header("Everything is an Algorithm")
 st.write("Use the sidebar on the left to navigate across different modules!")
+st.write("This application mainly focus on writing and understanding ML models from its very base " \
+"along with its intutions and derivations")
 # st.write("Creator: D20")
 
 # st.metric(label="Model Accuracy", value="92%", delta="+3% from last build")
